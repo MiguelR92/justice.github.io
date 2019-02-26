3986d579ee30dd876d257417081949ddd3c6911c
Miguel Angel Guijarro Rodriguez <GURM9273@dssat.sat.gob.mx>
2019-02-26 14:06:39 -0600

    Intento de agragar funcion enviar

---
+++ enviar.php
@@ -0,0 +1,34 @@
+<?php
+   //Reseteamos variables a 0.
+   $nombre = $email = $mensaje = $para = $headers = $msjCorreo = NULL;
+
+   if (isset($_POST['submit'])) {
+      //Obtenemos valores input formulario
+      $nombre = $_POST['name'];
+      $email = $_POST['email'];
+      $mensaje = $_POST['message'];
+      $para = 'epzilonzeronorequiem@gmail.com';
+
+      //Creamos cabecera.
+      $headers = 'De' . " " . $email . "\r\n";
+      $headers .= "Content-type: text/html; charset=utf-8";
+
+      //Componemos cuerpo correo.
+      $msjCorreo = "Nombre: " . $nombre;
+      $msjCorreo .= "\r\n";
+      $msjCorreo .= "Email: " . $email;
+      $msjCorreo .= "\r\n";
+      $msjCorreo .= "Mensaje: " . $mensaje;
+      $msjCorreo .= "\r\n";
+
+    if (mail($para, $subject, $msjCorreo, $headers)) {
+         echo "<script language='javascript'>
+            alert('Mensaje enviado, muchas gracias.');
+         </script>";
+    } else {
+         echo "<script language='javascript'>
+            alert('fallado');
+         </script>";
+    }
+  }
+?>
